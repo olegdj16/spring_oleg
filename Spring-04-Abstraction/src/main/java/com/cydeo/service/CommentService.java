ba87b2539adda208160daad7e14ec2b8c2c1b17a
olegdj16 <92351568+olegdj16@users.noreply.github.com>
2023-03-10 21:10:24 -0600

    commit

--- Spring-04-Abstraction/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-04-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -3,13 +3,16 @@ package com.cydeo.service;
 import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CommentService {
 
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
 
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
