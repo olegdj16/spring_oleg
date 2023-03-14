565bbf0dc913752e36915cf374c04972dc2409dc
olegdj16 <92351568+olegdj16@users.noreply.github.com>
2023-03-14 01:24:22 -0500

    Spring MVC - Model

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/MentorController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/MentorController.java
@@ -2,23 +2,25 @@ package com.cydeo.controller;
 
 import com.cydeo.enums.Gender;
 import com.cydeo.model.Mentor;
-import com.cydeo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+
 @Controller
+@RequestMapping("/mentor")
 public class MentorController {
-    @RequestMapping("/list")
+
+    @RequestMapping("/list") // localhost:8080/mentor/list
     public String showTable(Model model){
 
         List<Mentor> mentorList = new ArrayList<>();
         mentorList.add(new Mentor("Mike", "Smith", 45, Gender.MALE));
         mentorList.add(new Mentor("Tom", "Hanks", 65, Gender.MALE));
         mentorList.add(new Mentor("Ammy", "Bryan", 25, Gender.FEMALE));
+
         model.addAttribute("mentors", mentorList);
 
         return "/mentor/mentor-list";
