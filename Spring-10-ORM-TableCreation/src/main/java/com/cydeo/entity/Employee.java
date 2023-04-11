17813b828c237aba003b87b14811c7a456c130f3
olegdj16 <92351568+olegdj16@users.noreply.github.com>
2023-04-11 02:47:48 -0500

    commit

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Employee.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Employee.java
@@ -1,16 +1,18 @@
 package com.cydeo.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity //creates a table through hibernate
-public class Employee {
+@Entity
+public class Employee {  //employee
 
     @Id
-    private int id;
-    private String firstName;
-    private String lastName;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String name;
 
 }
-
-
