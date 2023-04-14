9d7c751ea2e23e23800fb5cc7979f124ebf26129
olegdj16 <92351568+olegdj16@users.noreply.github.com>
2023-04-14 14:48:04 -0500

    commit Car, day2

---
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/repository/CarRepository.java
@@ -0,0 +1,13 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepository extends JpaRepository<Car, Long> {
+
+    //custom methods
+    //sql methods
+
+
+
+}
