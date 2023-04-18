49b8271ca0d8a083c89693e141c66b03e4f4a128
olegdj16 <92351568+olegdj16@users.noreply.github.com>
2023-04-17 20:59:09 -0500

    commit

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Employee.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Employee.java
@@ -1,16 +1,17 @@
 package com.cydeo.entity;
 
-import javax.persistence.*;
 import com.cydeo.enums.Gender;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "employees")
 @Data
 @NoArgsConstructor
-public class Employee extends BaseEntity{
+public class Employee extends BaseEntity {
 
     private String firstName;
     private String lastName;
@@ -20,6 +21,15 @@ public class Employee extends BaseEntity{
     private int salary;
     @Enumerated(EnumType.STRING)
     private Gender gender;
+
+    @OneToOne(cascade = CascadeType.ALL)
+//    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.REMOVE})
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "region_id")
+    private Region region;
 
     public Employee(String firstName, String lastName, String email, LocalDate hireDate, int salary, Gender gender) {
         this.firstName = firstName;
