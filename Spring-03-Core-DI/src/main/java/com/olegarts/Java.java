b3574ee5113771dcd48a3d6c04b00b3be8f9df5a
olegdj16 <oleg16@gmail.com>
2025-03-02 02:16:47 -0600

    Autowired explanation

--- Spring-03-Core-DI/src/main/java/com/olegarts/Java.java
+++ Spring-03-Core-DI/src/main/java/com/olegarts/Java.java
@@ -1,25 +1,32 @@
 package com.olegarts;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java {
 
     // Field Injection
-    //  @Autowired
+    //  @Autowired <--- being Deprecated, will not use it
     //  OfficeHours officeHours;
 
     //Constructor Injection
     OfficeHours officeHours;
 
-//    @Autowired
-//    public Java(OfficeHours officeHours) {
-//        this.officeHours = officeHours;
-//    }
+    /**
+     * Starting with Spring 4.3, if a class has only one constructor,
+     * the @Autowired annotation is optional.
+     * However, if there are multiple constructors, you must explicitly
+     * specify the constructor to be used with the @Autowired annotation.
+     */
 
-    
+    // @Autowired
+    public Java(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+
     public void getTeachingHours(){
-        System.out.println("Weekly teaching hours : 15" + (20 + officeHours.getHours()));
+        System.out.println("Weekly teaching hours : " + (20 + officeHours.getHours()));
 
     }
 }
