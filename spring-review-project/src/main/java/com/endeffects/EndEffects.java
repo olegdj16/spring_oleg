30e47b9e2f6b7d5ac5dc9b8d5b749dbd09db3075
olegdj16 <oleg16@gmail.com>
2025-04-20 22:15:47 -0500

    SKYN-9 - fixing bugs

--- spring-review-project/src/main/java/com/endeffects/EndEffects.java
+++ spring-review-project/src/main/java/com/endeffects/EndEffects.java
@@ -2,6 +2,7 @@ package com.endeffects;
 
 import com.endeffects.config.EmployeeConfig;
 import com.endeffects.repository.HoursRepository;
+import com.endeffects.service.OvertimeSalaryService;
 import com.endeffects.service.SalaryService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,14 +10,22 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class EndEffects {
     public static void main(String[] args) {
 
-        // we need to create a container
+        // Create Spring container
         ApplicationContext container = new AnnotationConfigApplicationContext(EmployeeConfig.class);
 
+        // Get regular salary service and calculate
         SalaryService salaryService = container.getBean(SalaryService.class);
         salaryService.calculateRegularSalary();
 
-        HoursRepository hr = container.getBean(HoursRepository.class);
-        System.out.println(hr);
+        // Get overtime salary service and calculate
+        OvertimeSalaryService overtimeSalaryService = container.getBean(OvertimeSalaryService.class);
+        overtimeSalaryService.calculateOvertimeSalary();
 
+        // If you really want to print one of the HoursRepository beans:
+        HoursRepository regHours = (HoursRepository) container.getBean("regularHours");
+        System.out.println("Regular hours repo: " + regHours);
+
+        HoursRepository otHours = (HoursRepository) container.getBean("overtimeHours");
+        System.out.println("Overtime hours repo: " + otHours);
     }
 }
